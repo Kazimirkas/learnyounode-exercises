1a3a20e710d687df4964c09acc91f1af967a5257
Kazimirkas <vyperdos@gmail.com>
2016-12-20 17:40:32 +0200

    done

--- apiserver.js
+++ apiserver.js
@@ -1,5 +1,7 @@
 const url = require('url');
 const http = require('http');
+const timeUrl = '/api/parsetime';
+const unixUrl = '/api/unixtime';
 const server = http.createServer((request, response) => {
   if (request.method !== 'GET') {
     response.end('send me a GET\n');
@@ -7,8 +9,19 @@ const server = http.createServer((request, response) => {
 
   const { pathname, query } = url.parse(request.url, true);
 
-  if (pathname === '/api/parsetime' && !!query.iso) {
-    console.log(pathname, query);
+  if ((pathname === timeUrl || pathname === unixUrl) && !!query.iso) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    const date = new Date(query.iso);
+    const res = pathname === timeUrl ?
+    ({
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
+    }) : ({
+      unixtime: date.getTime()
+    });
+
+    response.end(JSON.stringify(res));
   }
 
 });
